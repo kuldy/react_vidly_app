fe90efdec57da93190783bcd7513d9315aa283cd
kuldeep <kuldykld@gmail.com>
2021-08-26 18:27:23 +0530

    Implemented register form

--- src/vidly/components/Register.js
+++ src/vidly/components/Register.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
+import Form from "./common/Form";
 
-class Register extends Component {
-  state = {};
+class Register extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().email().required().label("Email"),
+    password: Joi.string().min(5).required().label("Password"),
+    name: Joi.string().required().label("Name"),
+  };
+
+  doSubmit = () => {
+    console.log("Register Form Submitted");
+  };
   render() {
-    return <h1>Register</h1>;
+    return (
+      <div>
+        <form className="col-md-6 offset-md-3" onSubmit={this.handleSubmit}>
+          <h1 className="text-center">
+            <span className="text-primary"> Register</span> Form
+          </h1>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
   }
 }
 
