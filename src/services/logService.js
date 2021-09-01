9fb4db1f22526e436e4e00b8908fba0fcffc34b9
kuldeep <kuldykld@gmail.com>
2021-09-01 18:38:44 +0530

    installed scentry logging service, and created a logging service module

---
+++ src/services/logService.js
@@ -0,0 +1,23 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn: "https://a8606420e9374a7b87852073723cd585@o984570.ingest.sentry.io/5940377",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log,
+};
