1288107d68012cba53a7e7f09d2ce66b5101fd87
kuldeep <kuldykld@gmail.com>
2021-09-06 10:32:44 +0530

    changed MoviesApp component from function to class. and fetch token from local storage and set the user to state of the component

--- src/vidly/MoviesApp.js
+++ src/vidly/MoviesApp.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import jwtDecode from "jwt-decode";
 
 import Movies from "./components/Movies";
 import Customers from "./components/Customers";
@@ -11,24 +12,38 @@ import MovieForm from "./components/MovieForm";
 import LoginFormWithJoi from "./components/LoginFormWithJoi";
 import RegisterForm from "./components/RegisterForm";
 
-const MoviesApp = () => {
-  return (
-    <>
-      <ToastContainer />
-      <NavBar />
-      <Switch>
-        <Route path="/register" component={RegisterForm} />
-        <Route path="/login" component={LoginFormWithJoi} />
-        <Route path="/movies/:id" component={MovieForm} />
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </>
-  );
-};
+class MoviesApp extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const { data } = jwtDecode(jwt);
+      this.setState({ user: data });
+    } catch (error) {
+      {
+      }
+    }
+  }
+  render() {
+    return (
+      <>
+        <ToastContainer />
+        <NavBar />
+        <Switch>
+          <Route path="/register" component={RegisterForm} />
+          <Route path="/login" component={LoginFormWithJoi} />
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </>
+    );
+  }
+}
 
 export default MoviesApp;
