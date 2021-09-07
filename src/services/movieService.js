1b80a184c829a68522c3a8f5c9e0b7a2110e5bad
kuldeep <kuldykld@gmail.com>
2021-09-07 12:52:36 +0530

    refactored code improved backend routes to rest resource routes and changed movieService accordingly

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,26 +1,28 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
+const endPoint = "/movies";
+
 async function getMovies() {
-  const { data } = await http.get(apiUrl + `/movies`);
+  const { data } = await http.get(apiUrl + endPoint);
   return data.movies;
 }
 
 async function deleteMovie(movieId) {
-  return await http.delete(apiUrl + `/delete-movie/${movieId}`);
+  return await http.delete(apiUrl + `${endPoint}/${movieId}`);
 }
 
 async function getMovie(movie_id) {
-  const { data } = await http.get(apiUrl + `/view-movie/${movie_id}`);
+  const { data } = await http.get(apiUrl + `${endPoint}/${movie_id}`);
   return data;
 }
 
-function saveMovie(movie) {
+async function saveMovie(movie) {
   // console.log("submitted movie object is:", movie);
   if (movie.id) {
-    return http.put(apiUrl + `/update-movie/${movie.id}`, movie);
+    return http.put(apiUrl + `${endPoint}/${movie.id}`, movie);
   }
-  return http.post(apiUrl + `/add-movie`, movie);
+  return http.post(apiUrl + endPoint, movie);
 }
 
 export { getMovies, getMovie, saveMovie, deleteMovie };
